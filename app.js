14b2f51e575cf7a49649d27ec412b94d3f47e2ed
rohit khurana <rohitkhurana26697@gmail.com>
2017-12-10 08:43:01 +0000

    add mlab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes=require("./routes/comments"),
     authRoutes=require("./routes/index"),
     campgroundRoutes=require("./routes/campgrounds");
     
-mongoose.connect("mongodb://localhost/yelpcampv12");
+//mongoose.connect("mongodb://localhost/yelpcampv12");
+mongoose.connect("mongodb://rohit:rohit@ds135926.mlab.com:35926/yelpcamp");
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
